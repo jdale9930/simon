c40c4420ff668550901917ef3d5deebe5cb50522
Joshua Dale <jdale9930@gmail.com>
2021-01-30 15:55:00 -0600

    I have continued to not add commits, dammit

--- simon.js
+++ simon.js
@@ -5,48 +5,23 @@ let sequence = [];
 let lastPress = ""
 let currentStreak = 0;
 let highestStreak = 0;
+let glowInterval = 1;
+let idleStop = 0;
+let whileStop = 0;
+let sequenceLoc = 0;
 function startGame()
 {
     addListeners()
     console.log("yes")
 
-    while(streak == true)
-    {
+    //while(streak == true && whileStop == 0)
+    //{   
+        whileStop = 1;
+        console.log('enter while')
         on = false;
         lastPress = "";
         addToSequence()
-        for(let i = 0; i < sequence.length; i++)
-        {
-            document.getElementById(sequence[i]).classList.add('glow');
-            let end = window.setTimeout(()=> {
-                document.getElementById(sequence[i]).classList.remove('glow');
-            }, 2000)
-        }
-
-        on = true;
-        if(lastPress != "")
-        {
-            for(let i = 0; i < sequence.length; i++)
-            {
-                if(lastPress === sequence[i])
-                {
-
-                }
-                else
-                {
-                    streak = false;
-                    if(currentStreak > highestStreak)
-                    {
-                        
-                    }
-                    currentStreak = 0;
-                    
-                    break
-                }
-            }
-        }
-        
-    }
+        playSequence()
 
 }
 
@@ -59,6 +34,50 @@ function addListeners()
         {
         lastPress = e.target.id
         console.log(lastPress)
+
+        //for(let i = 0; i < sequence.length; i++)
+            //{
+                if(lastPress === sequence[sequenceLoc])
+                {
+                    //on = false;
+                    document.getElementById('center').classList.add('correct');
+                    let end = window.setTimeout(() =>{
+                        document.getElementById('center').classList.remove('correct');
+                    }, 100)
+                    //on = true;
+                    whileStop = 0;
+                    if(sequenceLoc + 1 < sequence.length)
+                    {
+                        console.log('entered if')
+                        sequenceLoc = sequenceLoc + 1;
+                    }
+                    else
+                    {
+                        console.log('entered else')
+                        sequenceLoc = 0;
+                        currentStreak = currentStreak + 1
+                        document.getElementById('streak').innerText = currentStreak;
+                        addToSequence();
+                        playSequence();
+                    }
+                }
+                else
+                {
+                    document.getElementById('center').classList.add('correct');
+                    if(currentStreak > highestStreak)
+                    {
+                        document.getElementById('highStreak').innerText = currentStreak;
+                    }
+                    currentStreak = 0;
+                    on = false;
+                    streak = false;
+                    sequenceLoc = 0;
+                    //break
+                }
+            //}
+        //addToSequence()
+        //playSequence()
+        //return press;
         }
     })
 }
@@ -79,12 +98,31 @@ function addToSequence()
 
         if(random == 3)
         {
-            sequence.push("yellow")
+            sequence.push("green")
         }
 
         if(random == 4)
         {
             sequence.push("blue")
         }
+        console.log(sequence)
+    }
 
+    function playSequence()
+    {
+        on = false;
+        for(let i = 0; i < sequence.length; i++)
+        {
+            let start = window.setTimeout(()=> {
+                document.getElementById(sequence[i]).classList.add('glow');
+                }, i * 1500 + 1500);
+            //document.getElementById(sequence[i -1]).classList.add('glow');
+                let end = window.setTimeout(()=> {
+                document.getElementById(sequence[i]).classList.remove('glow');
+                }, i * 1500 + 2000);
+        }
+        let onTimer = window.setTimeout(() =>{
+            on = true;
+            console.log(on)
+        }, 1500 * sequence.length + 500)
     }
